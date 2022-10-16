439dbac6384bd7da63ca1878b106dfb3e52db1b6
williamsparell <williamatlasnova@gmail.com>
2022-10-16 15:53:35 +0200

    handler

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,82 @@ const numClickHandler = (e) => {
       res: !calc.sign ? 0 : calc.res,
     });
   }
-}
+};
+
+const signClickHandler = (e) => {
+  e.preventDefault();
+  const value = e.target.innerHTML;
+
+  setCalc({
+    ...calc,
+    sign: value,
+    res: !calc.res && calc.num ? calc.num : calc.res,
+    num: 0,
+  });
+};
+
+const equalsClickHandler = () => {
+  if (calc.sign && calc.num) {
+    const math = (a, b, sign) =>
+      sign === "+"
+        ? a + b
+        : sign === "-"
+        ? a - b
+        : sign === "X"
+        ? a * b
+        : a / b;
+
+    setCalc({
+      ...calc,
+      res:
+        calc.num === "0" && calc.sign === "/"
+          ? "Can't divide with 0"
+          : math(Number(calc.res), Number(calc.num), calc.sign),
+      sign: "",
+      num: 0,
+    });
+  }
+};
+
+const invertClickHandler = () => {
+  setCalc({
+    ...calc,
+    num: calc.num ? calc.num * -1 : 0,
+    res: calc.res ? calc.res * -1 : 0,
+    sign: "",
+  });
+};
+
+const commaClickHandler = (e) => {
+  e.preventDefault();
+  const value = e.target.innerHTML;
+
+  setCalc({
+    ...calc,
+    num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+  });
+};
+
+const percentClickHandler = () => {
+  let num = calc.num ? parseFloat(calc.num) : 0;
+  let res = calc.res ? parseFloat(calc.res) : 0;
+
+  setCalc({
+    ...calc,
+    num: (num /= Math.pow(100, 1)),
+    res: (res /= Math.pow(100, 1)),
+    sign: "",
+  });
+};
+
+const resetClickHandler = () => {
+  setCalc({
+    ...calc,
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+};
 
 function App() {
     let [calc, setCalc] = useState({
