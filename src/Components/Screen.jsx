b19b3a14cd8c4e3f54b0f557223a6a3053bb2378
williamsparell <williamatlasnova@gmail.com>
2022-10-18 12:04:41 +0200

    calc

--- src/Components/Screen.jsx
+++ src/Components/Screen.jsx
@@ -1,11 +1,12 @@
+import { useContext } from "react";
 import { Textfit } from "react-textfit";
+import { CalcContext } from "../Context/CalcContext";
 import "./Screen.css";
 
-const Screen = ({ value }) => {
+const Screen = () => {
+    const { calc } = useContext(CalcContext);
     return (
-        <Textfit className="screen" mode="single" max={70}>
-            {value}
-        </Textfit>
+        <Textfit className="screen" max={70} mode="single">{calc.num ? calc.num : calc.res}</Textfit>
     )
 }
 
